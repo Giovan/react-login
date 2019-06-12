8858dcf1e6c7c3483517e56773ae48d525fd273e
Giovanni <giovanniyatze@gmail.com>
2019-06-12 02:47:33 -0500

    Add login component

---
+++ src/components/login.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Login.sass";
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="Login">
+        
+      </div>
+    );
+  }
+}
+
+export default Login;
